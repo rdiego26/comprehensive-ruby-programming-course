682bdab79bf229cf5d5b0342bcc359093bd5755f
ramos <diego.ramos@pipefy.com>
2022-12-30 23:29:49 +0000

    add: numbers

---
+++ 3_numbers/arithmetic_functions.rb
@@ -0,0 +1,11 @@
+p "1+1=#{1+1}"
+p "1*1=#{1*1}"
+p "10/2=#{10/2}"
+p "10**2=#{10**2}"
+
+
+# Please Excuse My Dear Aunt Sally
+# PEMDAS
+# Parentheis, Exponents, Multiplication, Division, Addition, Subtraction
+
+p "5 + 15 * 20 - 2 / 6**3 - (3 +1)=#{5 + 15 * 20 - 2 / 6**3 - (3 +1)}"
